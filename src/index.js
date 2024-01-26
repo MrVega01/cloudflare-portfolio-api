e800e10bc2a46a4029c66f4872c719875d81d4ae
MrVega01 <ignaciovega2003@outlook.com>
2024-01-26 19:16:47 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
+// send email
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+		const url = new URL(request.url)
+
+		if (url.pathname === "/email" && request.method === "POST") {
+			try {
+				const { subject, content } = await request.json() || {} // BODY
+				if (!subject || !content) return Response.json({ error: 'Bad request' }, { status: 400 })
+				
+				const bodyRequest = {
+					personalizations: [
+						{
+							to: [
+								{
+									"email": "ignaciovega200301@gmail.com"
+								}
+							]
+						}
+					],
+					from: {
+						email: "ignaciovega200301@gmail.com"
+					},
+					subject,
+					content: [
+						{
+							type: "text/html",
+							value: content
+						}
+					]
+				}
+				
+				const { API_KEY_SENDGRID } = env
+				return fetch('https://api.sendgrid.com/v3/mail/send', {
+					method: 'POST',
+					headers: {
+						Authorization: `Bearer ${API_KEY_SENDGRID}`,
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify(bodyRequest)
+				}).then(async (response) => {
+						return new Response(null, { status: 200 })
+					})
+			} catch (error) {
+				console.error(error)
+				return Response.json({ error: 'Server error' }, { status: 502 })
+			}
+		}
+		return new Response('Not found', { status: 404 })
+	}
+}
