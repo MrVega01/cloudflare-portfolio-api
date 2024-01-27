bc459af1e15fcf0a69c3fa7d95da238f9bb12c16
MrVega01 <ignaciovega2003@outlook.com>
2024-01-27 01:16:28 -0400

    Add CORS & auth verification

--- src/index.js
+++ src/index.js
@@ -3,10 +3,41 @@ export default {
 	async fetch(request, env, ctx) {
 		const url = new URL(request.url)
 
+		const headers = new Headers();
+		headers.set("Access-Control-Allow-Origin", "*")
+		headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, POST, DELETE, OPTIONS")
+		headers.set("Access-Control-Allow-Headers", request.headers.get("Access-Control-Request-Headers"))
+		headers.set("Access-Control-Max-Age", "86400")
+
+    if (request.method === 'OPTIONS') {
+			if (
+        request.headers.get("Origin") !== null &&
+        request.headers.get("Access-Control-Request-Method") !== null &&
+        request.headers.get("Access-Control-Request-Headers") !== null
+      ) {
+        // Handle CORS preflight requests.
+        return new Response(null, { headers })
+      } else {
+        // Handle standard OPTIONS request.
+        return new Response(null, {
+          headers: {
+            Allow: "GET, HEAD, POST, OPTIONS",
+          },
+        });
+      }
+    }
+    else {
+			const auth = request.headers.get('Authorization')
+			if(auth){
+				if(auth !== env.API_KEY) return new Response(null, { status: 403, headers })
+			} else return new Response(null, { status: 401, headers })
+		}
+
 		if (url.pathname === "/email" && request.method === "POST") {
 			try {
-				const { subject, content } = await request.json() || {} // BODY
-				if (!subject || !content) return Response.json({ error: 'Bad request' }, { status: 400 })
+				const body = await request.json() || {} // BODY
+				const { subject, content } = body
+				if (!subject || !content) return Response.json({ error: 'Bad request' }, { status: 400, headers })
 				
 				const bodyRequest = {
 					personalizations: [
@@ -38,14 +69,14 @@ export default {
 						'Content-Type': 'application/json'
 					},
 					body: JSON.stringify(bodyRequest)
-				}).then(async (response) => {
-						return new Response(null, { status: 200 })
+				}).then(async () => {
+						return new Response(null, { status: 200, headers })
 					})
 			} catch (error) {
-				console.error(error)
-				return Response.json({ error: 'Server error' }, { status: 502 })
+				console.error({error, body})
+				return Response.json({ error: 'Server error' }, { status: 500, headers })
 			}
 		}
-		return new Response('Not found', { status: 404 })
+		return new Response('Not found', { status: 404, headers })
 	}
 }
